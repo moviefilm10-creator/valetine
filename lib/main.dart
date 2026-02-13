b41701f363c563fa4c5c5c01318297258a6ce79e
moviefilm10-creator <moviefilm10@gmail.com>
2026-02-13 23:22:02 +0100

    Create main.dart file with Flutter app entry point and Firebase initialization

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Valetine',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Valetine Home'),
+        ),
+        body: Center(
+          child: Text('Welcome to Valetine!'),
+        ),
+      ),
+    );
+  }
+}
